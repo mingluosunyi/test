b5bbaf5f006bc4de03904e0dc7cf8a305d81509d
mingluo <mingluosunyi@icloud.com>
2022-09-05 12:28:23 +0800

    added SomeButtons component file and fix a bug in HelloWorld.vue

---
+++ src/components/SomeButtons.vue
@@ -0,0 +1,14 @@
+<template>
+    <button>Cancel</button>
+    <button>Confirm</button>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
